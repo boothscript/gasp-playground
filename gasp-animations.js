18f6b29e9c164191b2144390377e424ccb9a6e0c
Steve Booth <scriptmcuk@gmail.com>
2021-01-28 10:15:45 +0000

    Add slider animation

--- gasp-animations.js
+++ gasp-animations.js
@@ -1,3 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
 tl.to('#intro-text', { y: '0%', duration: 1, stagger: 0.5 });
+tl.to('#slider', { y: '-100%', duration: 1, delay: 1 });
+tl.to('#intro-background', { y: '-100%', duration: 1 }, '-=1');
