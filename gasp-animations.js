a214c6e149bdae0c0ce6734397c196ba40cb8a34
Steve Booth <scriptmcuk@gmail.com>
2021-01-29 12:03:48 +0000

    added page styling

--- gasp-animations.js
+++ gasp-animations.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-tl.to('#intro-text', { y: '0%', duration: 1, stagger: 0.5 });
+tl.to('#intro-text', { y: '0%', duration: 0.8, stagger: 0.2 });
 tl.to('#slider', { y: '-100%', duration: 1, delay: 1 });
 tl.to('#intro-background', { y: '-100%', duration: 1 }, '-=1');
